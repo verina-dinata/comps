9ca9a3e6a549d18ba79165137662a669dd4b9b40
Verina Ong Dinata <verina.od1997@gmail.com>
2022-11-29 16:53:29 +0800

    Add styling using tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import Button from "./Button";
+import Button from './Button';
 
 function App() {
   return (
     <div>
       <div>
-        <Button></Button>
+        <Button success rounded outline>
+          Click me!!
+        </Button>
       </div>
       <div>
-        <Button></Button>
+        <Button danger outline>
+          Buy Now!
+        </Button>
       </div>
       <div>
-        <Button></Button>
+        <Button warning>See Deal!</Button>
       </div>
       <div>
-        <Button></Button>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
       </div>
       <div>
-        <Button></Button>
+        <Button secondary rounded>
+          Something!
+        </Button>
       </div>
     </div>
   );
