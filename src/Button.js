b497a0a8223cb4a33759d9dcaedbfea8397a37aa
Verina Ong Dinata <verina.od1997@gmail.com>
2022-11-29 17:13:22 +0800

    use classnames for button styling

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import className from 'classnames';
 
 function Button({
   children,
@@ -10,7 +10,22 @@ function Button({
   outline,
   rounded
 }) {
-  return <button className='px-3 py-1.5 border border-blue-600 bg-blue-500 text-white'>{children}</button>;
+  const classes = className('px-3 py-1.5 border', {
+    'border-blue-500 bg-blue-500 text-white': primary,
+    'border-gray-500 bg-gray-500 text-white': secondary,
+    'border-green-500 bg-green-500 text-white': success,
+    'border-yellow-500 bg-yellow-500 text-white': warning,
+    'border-red-500 bg-red-500 text-white': danger,
+    'rounded-full': rounded,
+    'bg-white': outline,
+    'text-blue-500': outline && primary,
+    'text-gray-500': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-500': outline && warning,
+    'text-red-500': outline && danger,
+  });
+
+  return <button className={classes}>{children}</button>;
 };
 
 Button.propTypes = {
