4fd708fa40c3b62109c2ca8f846a8e4c787aaa9c
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-03 15:33:57 +0800

    add id to items list

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: 'asdaed',
       label: 'Can I use React on a project?',
       content: 'You can use React on any project you want'
     },
     {
+      id: 'w3rnf',
       label: 'Can I use Javascipt on a project?',
       content: 'You can use Javascipt on any project you want'
     },
     {
+      id: 'sijudfgn',
       label: 'Can I use Python on a project?',
       content: 'You can use Python on any project you want'
     }
   ];
 
-  return <Accordion />;
+  return <Accordion items={items} />;
 }
 
 export default App;
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,16 @@
-function Accordion() {
+function Accordion({ items }) {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
-
+      {renderedItems}
     </div>
   );
 }
