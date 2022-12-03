19e3d7c4ea65bd63f0a905420dce42e5eca95326
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-03 17:17:42 +0800

    add onClick event handler

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpendedItems] = useState(2);
+  const [expandedIndex, setExpandedIndex] = useState(2);
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
-    // React doesn't print True, false, undefined, null
-    // JS && will print the 1st falsy value or the last truthy value
-
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        {/* // React doesn't print True, false, undefined, null
+        // JS && will print the 1st falsy value or the last truthy value */}
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
