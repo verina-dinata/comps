d28cfcaa9e29aded94950ec56dfd589e5b7c2879
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-03 21:53:15 +0800

    add event handler and state function for selecting an option

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Dropdown from "./components/Dropdown";
+import { useState } from 'react';
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (newOption) => {
+    setSelection(newOption);
+  };
+
   const options = [
     { label: 'Red', value: 'red' },
     { label: 'Blue', value: 'blue' },
     { label: 'Green', value: 'green' },
   ]
 
-  return <Dropdown options={options} />;
+  return <Dropdown
+    options={options}
+    onSelect={handleSelect}
+    selection={selection} />;
 }
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,23 +1,36 @@
 import { useState } from 'react'
 
-function Dropdown({ options }) {
+function Dropdown({ options, onSelect, selection }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    // setIsSelected(option.label)
+    onSelect(option);
+  }
+
   const renderedOptions = options.map((option) => {
     return(
-      <div key={option.value}>
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
         {option.label}
       </div>
     );
   });
 
+  let content = 'Select...';
+  if (selection) {
+    content = selection.label
+  }
+
   return(
     <div>
-      <div onClick={handleClick}>Select ...</div>
+      <div onClick={handleClick}>
+        {content}
+      </div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
   );
