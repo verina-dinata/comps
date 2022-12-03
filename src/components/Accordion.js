e1ff72cb8fdf8efd89a3f20b55bb486a9c3fbd1e
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-03 17:27:27 +0800

    move event handler outside of map function for better readability

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,16 @@ import { useState } from 'react';
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(2);
 
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
         {/* // React doesn't print True, false, undefined, null
         // JS && will print the 1st falsy value or the last truthy value */}
         {isExpanded && <div>{item.content}</div>}
