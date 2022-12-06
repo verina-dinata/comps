1c19d1816aaf7d087a7127834ae9d4fde90e94f8
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-06 12:33:00 +0800

    add navigationProvider skeleton

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import './index.css'
+import { NavigationProvider } from './context/navigation.js';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
