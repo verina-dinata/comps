2e36d905ca60c6fa20553712f6ebc99699710570
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-06 12:04:05 +0800

    add dropdownPage.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from 'react';
-
 function App() {
-  const [selection, setSelection] = useState(null);
+  return <div></div>;
 
-  const handleSelect = (newOption) => {
-    setSelection(newOption);
-  };
-
-  const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Blue', value: 'blue' },
-    { label: 'Green', value: 'green' },
-  ]
-
-  return <Dropdown
-    options={options}
-    onChange={handleSelect}
-    value={selection} />;
 }
-
 export default App;
---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,23 @@
+import Dropdown from "../components/Dropdown";
+import { useState } from 'react';
+
+function DropdownPage() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (newOption) => {
+    setSelection(newOption);
+  };
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Blue', value: 'blue' },
+    { label: 'Green', value: 'green' },
+  ]
+
+  return <Dropdown
+    options={options}
+    onChange={handleSelect}
+    value={selection} />;
+}
+
+export default DropdownPage;
