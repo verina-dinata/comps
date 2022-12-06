7b041b8d76f191b9eb173e203b1549dbd66a16ba
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-06 17:31:26 +0800

    add styling for modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,18 @@
 import ReactDOM from 'react-dom';
 
-function Modal( { onClose }) {
+function Modal( { onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
       <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">
-        i'm a modal
+        <div className='flex flex-col justify-between h-full'>
+          {children}
+          <div className='flex justify-end'>
+            {actionBar}
+          </div>
+
+        </div>
+
       </div>
     </div>,
     document.querySelector('.modal-container')
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -13,10 +13,20 @@ function ModalPage() {
     setShowModal(false);
   }
 
+  const actionBar = <div>
+    <Button primary onClick={handleClose}>I accept</Button>
+  </div>
+
+  const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>
+        Here is an important agreement for you to accept
+      </p>
+    </Modal>
+
   return (
     <div>
       <Button primary onClick={handleClick}>Open modal</Button>
-      {showModal && <Modal onClose={handleClose}/>}
+      {showModal && modal}
     </div>
   );
 }
