9fe90d2d88736c21b5bdc59d4a790852ec17c16c
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-06 17:38:13 +0800

    disable scroll when modal is open

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
 function Modal( { onClose, children, actionBar }) {
+  useEffect (() => {
+    document.body.classList.add('overflow-hidden');
+
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    }
+  }, []);
+
   return ReactDOM.createPortal(
     <div>
       <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
@@ -10,9 +19,7 @@ function Modal( { onClose, children, actionBar }) {
           <div className='flex justify-end'>
             {actionBar}
           </div>
-
         </div>
-
       </div>
     </div>,
     document.querySelector('.modal-container')
