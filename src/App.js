f3de6ecabbbd9465507461c84f8ca0452e4a1dc1
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-06 13:15:31 +0800

    add route component to display the path the user clicks acordingly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Link from './components/Link';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
 
 function App() {
   return (
     <div>
       <Link to='/accordion'>Got to accodion</Link>
       <Link to='/dropdown'>Got to dropdown</Link>
+      <div>
+        <Route path='/accordion'>
+          <AccordionPage />
+        </Route>
+        <Route path='/dropdown'>
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 
---
+++ src/components/Route.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+function Route({ path, children }) {
+  const { currentPath } = useContext(NavigationContext);
+  if (currentPath === path) {
+    return children;
+  }
+
+  return null;
+}
+
+export default Route;
