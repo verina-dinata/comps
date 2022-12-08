37e7de94d11483a8037e7227fa6a784c41a4e483
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-08 22:15:37 +0800

    add header property on config if column is sortable

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,18 @@
 import Table from "./Table";
 
 function SortableTable(props) {
-  return <Table  {...props}/>
+  const { config } = props;
+
+  const upadtedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => <th>{column.label} IS SORTAABLE</th>
+    }
+  });
+  return <Table  {...props} config={upadtedConfig}/>
 };
 
 export default SortableTable;
