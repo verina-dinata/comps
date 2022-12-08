f4b83665cc544a6fd71828401e76957c9cd16800
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-08 22:00:39 +0800

    add sortValue to config

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
+// import Table from "../components/Table";
 
 function TablePage() {
   const data = [
@@ -11,7 +12,8 @@ function TablePage() {
   const config = [
     {
       label: 'Name',
-      render: (fruit) => fruit.name
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name
     },
     {
       label: 'Color',
@@ -20,7 +22,7 @@ function TablePage() {
     {
       label: 'Score',
       render: (fruit) => fruit.score,
-      header: () => <th>Score</th>
+      sortValue: (fruit) => fruit.score
     },
   ]
 
@@ -31,7 +33,7 @@ function TablePage() {
 
   return(
     <div>
-      <Table data={data} config={config} keyFn={keyFn}/>
+      <SortableTable data={data} config={config} keyFn={keyFn}/>
     </div>
   );
 }
