0be5ecad9aa5d57585102de420dfb122e9fbee69
Verina Ong Dinata <verina.od1997@gmail.com>
2022-12-10 15:54:08 +0800

    add onSubmit function for count

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,8 +1,10 @@
 import Button from '../components/Button';
 import { useState } from 'react';
+import Panel from '../components/Panel'
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -12,14 +14,37 @@ function CounterPage({ initialCount }) {
     setCount(count - 1);
   }
 
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    setValueToAdd(value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  }
+
   return (
-    <div>
-      <h1>
+    <Panel className='m-3'>
+      <h1 className='text-lg'>
         Count is {count}
       </h1>
-      <Button onClick={increment}>Increment</Button>
-      <Button onClick={decrement}>Decrement</Button>
-    </div>
+      <div className='flex flex-row'>
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          value={valueToAdd || ''}
+          onChange={handleChange}
+          type='number'
+          className='p-1 m-3 bg-gray-50 border border-gray-300' />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
 
